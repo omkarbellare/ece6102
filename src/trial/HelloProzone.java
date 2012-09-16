cf801d9a12e38ab23fad0b1a267c98faf3be1e09
Pranav Pandit <pppandit@gmail.com>
2012-09-16 16:26:23 -0400

    Added hub

--- src/trial/HelloProzone.java
+++ src/trial/HelloProzone.java
@@ -4,6 +4,6 @@ public class HelloProzone {
 	public static void main(String[] args) {
 		System.out.println("Mic Testing 1 2 3!");
 		System.out.println("All hail Prozone \\m/");
-		System.out.println("Testing the changelist functionality of Git");
+		System.out.println("Testing the changelist functionality of Github");
 	}
 }
