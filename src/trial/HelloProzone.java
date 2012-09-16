b265da1f74c646492e06a7ae8a9d4da00e245d2f
OMKAR BELLARE <o.bellare@gmail.com>
2012-09-16 15:14:11 -0400

    Adding another println

--- src/trial/HelloProzone.java
+++ src/trial/HelloProzone.java
@@ -4,5 +4,6 @@ public class HelloProzone {
 	public static void main(String[] args) {
 		System.out.println("Mic Testing 1 2 3!");
 		System.out.println("All hail Prozone \\m/");
+		System.out.println("Testing the changelist functionality of Git");
 	}
 }
