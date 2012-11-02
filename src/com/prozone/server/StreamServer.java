802aeba4b21a85470be272b156cd9efa3c7132e7
Pranav Pandit <pppandit@gmail.com>
2012-11-02 08:07:47 -0400

    Merging all the server functionality into a single file.

--- src/com/prozone/server/StreamServer.java
+++ src/com/prozone/server/StreamServer.java
@@ -1,21 +1,24 @@
 package com.prozone.server;
 
 
-import java.awt.Dimension;
-
-import java.net.DatagramPacket;
+import java.io.File;
+import java.io.IOException;
 import java.net.DatagramSocket;
+
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.TargetDataLine;
+import java.util.List;
+import java.util.Properties;
 
-import com.xuggle.mediatool.IMediaWriter;
+import com.prozone.driver.ProxyDriver;
+import com.prozone.http.NanoHTTPD;
+import com.prozone.http.NanoHTTPD.Response;
 
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
@@ -26,9 +29,12 @@ public class StreamServer {
 	private static final int STREAMIN_PORT = 9998;
 	private static final int STREAMOUT_PORT = 5555;
 
-	private String repo = "/home/neer/stream_files/";
+	private String repo = "/home/pranav/stream_files/";
 	private int writeIndex  = 0;
 	private int readIndex = 0;
+	
+	private static List streamingDevicesList;
+	private static String primaryDevice;
 
 
 	private String[] mediaOptions = {null,
@@ -42,6 +48,7 @@ public class StreamServer {
 	private InetAddress inetAddress;
 	private StreamWriter streamWriter;
 	private StreamReader streamReader;
+	private StreamingServer streamingServer;
 
 	private void constructURL(){
 		url = ":sout=#rtp{sdp=rtsp://@" + this.inetAddress.getHostAddress() + ":" + StreamServer.STREAMOUT_PORT+ "/demo,select=noaudio}";
@@ -132,94 +139,110 @@ public class StreamServer {
 		public void run() {
 			
 			while(true){
-				String outFileUrl = repo + "pandit"+ readIndex + ".mp4";
-				System.out.println("Writing new file");
-				player.prepareMedia(
-						  "http://192.168.1.109:8080/videofeed",
-						  ":sout=#transcode{vcodec=h264,venc=x264{cfr=16},scale=1,acodec=mp4a,ab=160,channels=2,samplerate=44100}:file{dst="+outFileUrl+"}",
-						  ":no-sout-rtp-sap", 
-						  ":no-sout-standard-sap", 
-						  ":sout-all", 
-						  ":sout-keep"
-						);
-				
-				player.start();
-			    try {
-					Thread.sleep(20000);
-					player.stop();
-				    //mediaPlayer.release();
-				} catch (InterruptedException e) {
-					System.out.println(e.getMessage());
+				System.out.println("Primary Device:"+primaryDevice);
+				if(!primaryDevice.equals("")) {
+					String outFileUrl = repo + "pandit"+ readIndex + ".mp4";
+					System.out.println("Writing new file");
+					player.prepareMedia(
+							"http://"+primaryDevice+":8080/videofeed",
+							":sout=#transcode{vcodec=h264,venc=x264{cfr=16},scale=1,acodec=mp4a,ab=160,channels=2,samplerate=44100}:file{dst="+outFileUrl+"}",
+							":no-sout-rtp-sap", 
+							":no-sout-standard-sap", 
+							":sout-all", 
+							":sout-keep"
+							);
+					player.start();
+					try {
+						Thread.sleep(20000);
+						player.stop();
+						//	mediaPlayer.release();
+					} catch (InterruptedException e) {
+						System.out.println(e.getMessage());
+					}
+					synchronized(StreamServer.class){
+						readIndex ++;
+						if(readIndex - writeIndex >= 2)
+							StreamServer.class.notify();
+					}
 				}
-			    
-				synchronized(StreamServer.class){
-					readIndex ++;
-					if(readIndex - writeIndex >= 2)
-						StreamServer.class.notify();
-				}
-
 			}
-
+		}
+	}
+	
+	public class StreamingServer extends NanoHTTPD implements Runnable {
+		
+		public StreamingServer(int port) throws IOException {
+			super(port, new File("."));
 		}
 
+		public Response serve( String uri, String method, Properties header, Properties parms, Properties files )
+		{
+			if(uri.equals("/heartbeat")) {
+				if(primaryDevice.equals("") && streamingDevicesList!=null && streamingDevicesList.size()>0) {
+					primaryDevice=(String) streamingDevicesList.get(0);
+					System.out.println("New primary device is:"+primaryDevice);
+				}
+				return new NanoHTTPD.Response( HTTP_OK, MIME_PLAINTEXT, "Ok" );
+			}
+			else if(uri.equals("/deviceList")) {
+				
+				streamingDevicesList.clear();
+				for(Object key:parms.keySet()) {
+					streamingDevicesList.add(parms.get(key));
+				}
+				//If the primary device was deregistered, elect a new primary
+				if(!streamingDevicesList.contains(primaryDevice) || primaryDevice.equals("")) {
+					if(streamingDevicesList.size()>0) {
+						primaryDevice=(String) streamingDevicesList.get(0);
+						System.out.println("New primary device is:"+primaryDevice);
+					}
+					else
+						primaryDevice="";
+				}
+				return new NanoHTTPD.Response( HTTP_OK, MIME_PLAINTEXT, "Ok" );
+			}
+			else {
+				System.out.println("URI="+uri);
+				return new NanoHTTPD.Response( HTTP_BADREQUEST, MIME_PLAINTEXT, "Bad Request" );
+			}
+		}
+
+		@Override
+		public void run() {
+			//This is to make this thread run forever
+			try {
+				System.in.read();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}	
 	}
 
-	public StreamServer(InetAddress inetAddress)
+	public StreamServer(InetAddress inetAddress) throws IOException
 	{
 		try {
-			//			this.inet4Address = (Inet4Address)inetAddress.;
+			streamingDevicesList=new ArrayList<String>();
+			primaryDevice="";
 			this.inetAddress = inetAddress;
 			socket = new DatagramSocket(STREAMIN_PORT, this.inetAddress);
 			socket.setSoTimeout(0); // blocking read
-
-
 			this.constructURL();
 			mediaOptions[0] = url;
-
 		} catch (SocketException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void startServer(){
+	public void startServer() throws IOException{
 		streamWriter  = this.new StreamWriter(mediaOptions);
 		streamReader = this.new StreamReader();
+		Properties prop=new Properties();
+		prop.load(ProxyDriver.class.getResourceAsStream("/config/config.properties"));
+		int server_port=Integer.parseInt(prop.getProperty("server_port"));
+		streamingServer = this.new StreamingServer(server_port);
 		
 		new Thread(streamReader).start();
 		new Thread(streamWriter).start();
+		new Thread(streamingServer).start();
 	}
-
-	public static void main(String[] args){
-		try {
-			NetworkInterface network = null;
-			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-			Collection<NetworkInterface> netsCollection = Collections.list(nets);
-			for(NetworkInterface net : netsCollection){
-				if(net.getDisplayName().equals("wlan0")){
-					network = net;
-					break;
-				}
-			}
-			
-			InetAddress inetAddress = null;
-			Enumeration<InetAddress> inets = network.getInetAddresses();
-			while (inets.hasMoreElements()) {
-				inetAddress= (InetAddress) inets.nextElement();
-				if(inetAddress instanceof Inet4Address)
-					break;
-
-			}
-
-			System.out.println(inetAddress.getHostAddress());
-			StreamServer ss = new StreamServer(inetAddress);
-			ss.startServer();
-		} catch (SocketException e) {
-			e.printStackTrace();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
 }
