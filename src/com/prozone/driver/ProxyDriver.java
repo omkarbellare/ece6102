b629660bbf2999fcd22557c0084978e91c71343d
Pranav Pandit <pppandit@gmail.com>
2012-11-24 10:07:57 -0500

    Adding change for proxy failure.

--- src/com/prozone/driver/ProxyDriver.java
+++ src/com/prozone/driver/ProxyDriver.java
@@ -2,7 +2,16 @@ package com.prozone.driver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.Properties;
 
 import com.prozone.proxy.HeartbeatHandler;
@@ -22,7 +31,27 @@ public class ProxyDriver {
 			int port_num=Integer.parseInt(prop.getProperty("proxy_port"));
 			int heartbeat_interval=Integer.parseInt(prop.getProperty("heartbeat_interval"));
 			int server_port=Integer.parseInt(prop.getProperty("server_port"));
-			StreamingProxyServer proxy=new StreamingProxyServer(port_num);
+			String listProxies[]=prop.getProperty("proxy_ips").split(",");
+			List<String> proxyList=Arrays.asList(listProxies);
+			
+			NetworkInterface network = null;
+			Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+			Collection<NetworkInterface> netsCollection = Collections.list(nets);
+			for(NetworkInterface net : netsCollection){
+				if(net.getDisplayName().equals("wlan0")){
+					network = net;
+					break;
+				}
+			}
+			InetAddress inetAddress = null;
+			Enumeration<InetAddress> inets = network.getInetAddresses();
+			while (inets.hasMoreElements()) {
+				inetAddress= (InetAddress) inets.nextElement();
+				if(inetAddress instanceof Inet4Address)
+					break;
+			}
+			System.out.println("Proxy running on IP:"+inetAddress.getHostAddress());
+			StreamingProxyServer proxy=new StreamingProxyServer(inetAddress.getHostAddress(),proxyList,port_num);
 			proxyThread=new Thread(proxy);
 			proxyThread.start();
 			HeartbeatHandler heartbeat=new HeartbeatHandler(heartbeat_interval,server_port);
